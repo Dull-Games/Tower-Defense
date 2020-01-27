1451076079de8471db92e000e49d8512789b8993
Nick4802 <nick4802@edu.sde.dk>
2020-01-27 17:19:10 +0100

    Changed constructor.

--- TowerDefenseLibrary/source/gameObjects/tower/Tower.cs
+++ TowerDefenseLibrary/source/gameObjects/tower/Tower.cs
@@ -2,18 +2,22 @@
 
 public class Tower
 {
-	string name;
-	string type;
-	int damage;
-	int range;
-	int speed;
+	private string name;
+	private string type;
+	private int damage;
+	private int range;
+	private int speed;
 
-	public Tower()
+	public Tower(string name, string type, int damage = 1, int range = 1, int speed = 1)
 	{
-
+		this.name = name;
+		this.type = type;
+		this.damage = damage;
+		this.range = range;
+		this.speed = speed;
 	}
 
-	void Fire()
+	public void Fire()
 	{
 
 	}
